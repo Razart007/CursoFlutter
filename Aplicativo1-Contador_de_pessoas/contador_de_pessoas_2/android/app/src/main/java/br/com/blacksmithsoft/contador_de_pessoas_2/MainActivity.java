eaa8b27f804cb7793ceba6d33a2d82d097d46297
Rafael Alves Cortes <rafael.redsky@gmail.com>
2020-02-23 18:28:57 -0300

    Primeiro aplicativo. Referente ao contador de pessoas. Foi utilizado nesse aplicativo: * StateFulWidget - Para utilizar o Hot Reload * Stack - Para Utilizar um component em cima de outro como layout * Children - Quando um widget contém vários filhos é necessário utilizar isso * Image.Asset - Utilização de imagens no aplicativo * Adição de assets e pasta de imagens * Column - Utilizado como o LinearLayout de forma vertical * alinhamento no centro com mainAxisAligment * Text - Texto padrão na tela é um label ou como se fosse o texview * Row - Utilizado como o LinearLayout de forma horizontal * Padding - Para regular espaçamentos entre widgets * FlatButton - Botões sem fundo

---
+++ Aplicativo1-Contador_de_pessoas/contador_de_pessoas_2/android/app/src/main/java/br/com/blacksmithsoft/contador_de_pessoas_2/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.blacksmithsoft.contador_de_pessoas_2;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
