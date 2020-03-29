9a58d2892f278992afbf81c1a8a055e1b26f9bb9
Rafael Alves Cortes <rafael.redsky@gmail.com>
2020-03-29 16:37:14 -0300

    * mais alterações da loja virtual

--- Aplicativo8-Loja_Virtual/loja_virtual/lib/screens/home_screen.dart
+++ Aplicativo8-Loja_Virtual/loja_virtual/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:lojavirtual/tabs/home_tab.dart';
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/tabs/products_tab.dart';
+import 'package:lojavirtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   final _pageController = PageController();
@@ -10,7 +12,20 @@ class HomeScreen extends StatelessWidget {
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        HomeTab()
+          Scaffold(
+            body: HomeTab(),
+            drawer: CustomDrawer(_pageController),
+          ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Produtos'),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+        Container(color: Colors.yellow,),
+        Container(color: Colors.green,),
       ],
     );
   }
